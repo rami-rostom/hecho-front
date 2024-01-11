258424df066224730b3e8f2f617e945dca62392d
Rami <rami.rostom@proton.me>
2024-01-11 15:14:26 +0100

    :sparkles: feat: init react project + install M

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createTheme, MantineProvider } from '@mantine/core';
 
 import App from './components/App/App';
 
+// Mantine styles
+import '@mantine/core/styles.css';
+
 import './styles/index.scss';
+
+// Define theme of the app with Mantine
+const theme = createTheme({
+  white: '#fff',
+  black: '#000',
+  primaryColor: 'indigo',
+  defaultRadius: 'sm',
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+    <MantineProvider defaultColorScheme="dark" theme={theme}>
+      <App />
+    </MantineProvider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
