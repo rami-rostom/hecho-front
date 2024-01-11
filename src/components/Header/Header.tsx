85d550a013f6ab112c66bc98b5fd6013486f934f
Rami <rami.rostom@proton.me>
2024-01-11 17:52:14 +0100

    :sparkles: feat: add logo + style tooltip

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,24 @@
-import { AppShell, Burger, Group } from '@mantine/core';
+import { AppShell, Burger, Group, Image, Anchor, Tooltip } from '@mantine/core';
+import './Header.scss';
 
 function Header({ opened, toggle }: { opened: boolean; toggle: () => void }) {
   return (
     <AppShell.Header>
-      <Group h="100%" px="md">
-        <div>Hecho</div>
+      <Group h="100%" px="md" className="header">
         <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        <Anchor href="/">
+          <Tooltip
+            label="Home"
+            position="right"
+            offset={8}
+            openDelay={300}
+            closeDelay={150}
+            transitionProps={{ transition: 'slide-left', duration: 200 }}
+            withArrow
+          >
+            <Image src="hecho-logo.png" h={30} />
+          </Tooltip>
+        </Anchor>
       </Group>
     </AppShell.Header>
   );
